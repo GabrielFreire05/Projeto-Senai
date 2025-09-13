c30a8325176a2bffb76440b3aa8b798603776d70
unknown <g6992281@gmail.com>
2025-09-13 14:20:10 -0300

    Commit inicial do projeto da calculadora 2

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const app = express(); // Instancia do express
 const PORT = 8081;
 
 const parseNumber = (numUmStr, numDoisStr) => {
-    const numUm = parseFloat(numUmStr);
-    const numDois = parseFloat(numDoisStr);
-    return { numUm, numDois };
+    const numUm = parseFloat(numUmStr);
+    const numDois = parseFloat(numDoisStr);
+    return { numUm, numDois };
 };
 
-app.get('/calcule/:operacao/:numUm/:numDois', (req, res) => {
-    const { operacao, numUm, numDois } = req.params;
+app.get('/calculadora', (req, res) => { 
+    
+
+    const { operacao, numUm, numDois } = req.query; 
+    
     let resultado;
-    const { numUm: numero1, numDois: numero2 } = parseNumber(numUm, numDois);
+    const numero1 = parseFloat(numUm);
+    const numero2 = parseFloat(numDois);
 
     if (isNaN(numero1) || isNaN(numero2)) {
         return res.status(400).json({ error: 'Parâmetro inválido. Por favor, insira um número válido.' });
@@ -21,6 +25,7 @@ app.get('/calcule/:operacao/:numUm/:numDois', (req, res) => {
         case 'soma':
             resultado = numero1 + numero2;
             break;
+
         case 'subtracao':
             resultado = numero1 - numero2;
             break;
@@ -28,21 +33,20 @@ app.get('/calcule/:operacao/:numUm/:numDois', (req, res) => {
         case 'multiplicacao':
             resultado = numero1 * numero2;
             break;
+
         case 'divisao':
             if (numero2 === 0) {
                 return res.status(400).json({ error: 'Divisão por zero não é permitida.' });
             }
+
             resultado = numero1 / numero2;
+            
             break;
         default:
             return res.status(400).json({ error: 'Operação inválida. Use soma, subtracao, multiplicacao ou divisao.' });
     }
     res.json({ resultado });
 });
-
-
-
-
 
 //Sempre a ultima linha do codigo
 app.listen(PORT, () => {
